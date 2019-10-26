f6cbf28680d3666ecae79f5e578fba616b8b9cf2
fed-gren <deveb22@gmail.com>
2019-10-26 23:57:33 +0900

    feat: add router

---
+++ router/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  fs.readFile(
+    path.join(__dirname, './public/html/index.htmlm'),
+    (_, data) => {
+      res.write(data);
+      res.end();
+    }
+  )
+});
+
+router.get('/api/', (req, res) => {
+  fs.readFile(path.join(__dirname, './api/data.json'),
+    (_, data) => {
+      res.write(data);
+      res.end();
+    })
+});
+
+module.exports = router;
