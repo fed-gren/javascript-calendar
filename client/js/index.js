d3d41369c75a43fcd3ca95da6f86fd0fde12e321
fed-gren <deveb22@gmail.com>
2019-10-30 02:20:17 +0900

    env: add production api url

--- client/js/index.js
+++ client/js/index.js
@@ -5,7 +5,7 @@ const calendarContainer_section = document.body.querySelector('.calendar');
 const calederOptions = {
   container: calendarContainer_section,
   width: '50rem',
-  eventApiUrl: process.env.EVENT_API_URL
+  eventApiUrl: process.env.PRODUCTION_API_URL || process.env.EVENT_API_URL
 }
 
 new Calendar({...calederOptions});
